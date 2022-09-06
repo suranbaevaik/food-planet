97d2984210dfe45c0388ee6af360c762ea019535
Aikanysh <suranbaeva.aik@gmail.com>
2022-09-06 23:36:48 +0600

    fix: cart page

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import burger from '../../assets/images/Burger1.png';
 import remove from '../../assets/icons/remove.png';
+import right_arrow from '../../assets/icons/right-arr.svg';
 import style from './Cart.module.css';
 
 const Cart = () => {
@@ -78,23 +79,26 @@ const Cart = () => {
                             <p>200 сом</p>
                         </td>
                         <td className={style.remove}>
-                            <img src={remove}  alt=""/>
+                            <img src={remove} alt=""/>
+                        </td>
+                    </tr>
+                    <tr className={style.sum_cell}>
+                        <td className={style.sum_title} colSpan="4">
+                            <p>Итого:</p>
+                        </td>
+                        <td colSpan="2">
+                            <p>400 сом</p>
                         </td>
                     </tr>
                     <tfoot>
                         <tr>
                             <td colSpan="3">
-                                <a href="#">Продолжить покупки</a>
+                                <a href="#">
+                                    <img className={style.rotate} src={right_arrow} alt=""/>
+                                    Продолжить покупки
+                                </a>
                             </td>
-                            <td className={style.sum_cell} >
-                                <div className={style.sum_number}>
-                                    <div>
-                                        <p>Итого:</p>
-                                    </div>
-                                    <div>
-                                        <p>200 сом</p>
-                                    </div>
-                                </div>
+                            <td colSpan="3" className={style.button_cell}>
                                 <div>
                                     <button type="submit">Оформить заказ</button>
                                 </div>
