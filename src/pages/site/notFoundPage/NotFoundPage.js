cb53e471c9479a6de9417df4a998e581424974b5
Aikanysh <suranbaeva.aik@gmail.com>
2022-10-11 19:34:58 +0600

    fix:

--- src/pages/site/notFoundPage/NotFoundPage.js
+++ src/pages/site/notFoundPage/NotFoundPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import Header from "../../../components/header/Header";
 import Footer from "../../../components/footer/Footer";
 import s from "./NotFoundPage.module.css";
@@ -10,7 +11,7 @@ const NotFoundPage = () => {
             <div className={s.container}>
                 <h1>404</h1>
                 <h2>Страница не найдена</h2>
-                <a href="/">Перейти на главную</a>
+                <Link to="/">Перейти на главную</Link>
             </div>
             <Footer/>
         </>
